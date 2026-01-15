14bf34fa453be5f1cb6b5c60f7c9d394fa6c106c
amit-singh-kanwal <amitkanwal19@gmail.com>
2026-01-15 22:57:32 +0530

    Trying to add javascript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("This is my new project ")
+console.log("This is guess the number project")
+let randomValue = 10;
+
+let guess = ()=>{
+    let input = document.getElementById('num');
+    let val =  input.value;
+    input.value = '';
+    let elem = document.getElementsByClassName('container')[0];
+    let insertElem = document.createElement('div');
+    insertElem.className = "new";    
+    if(val == randomValue){
+
+    } else{
+        if(val > randomValue){
+            insertElem.innerHTML = `<p>Your guess is too large</p>`;
+            elem.append(insertElem);
+        }else{
+            insertElem.innerHTML = `<p>Your guess is too small</p>`;
+            elem.append(insertElem);
+        }
+    }
+}
