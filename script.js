8ddaf3b76ef35563dc9028805f53e5f38ea79620
amit-singh-kanwal <amitkanwal19@gmail.com>
2026-01-17 19:39:00 +0530

    Final commit of the project

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 console.log("This is guess the number project")
-let randomValue = 10;
+function getRandomInt(max){
+    return Math.floor(Math.random() * (max +1));
+}
+let randomValue = getRandomInt(100);
+console.log(randomValue)
+
+let attempt = 0;
+const mxAttempt = 10;
+let arr = [];
+
+function showGuess(){
+    let element = document.getElementsByClassName("guessed")[0];
+    element.innerHTML = `You guessed: ${arr}`;
+}
 
 let guess = ()=>{
     let input = document.getElementById('num');
+    const elem = document.getElementsByClassName('result')[0];
+    elem.style.display = "block";
+    if(attempt >= mxAttempt){
+        input.value = "";
+        input.disabled = true;
+        elem.innerHTML = `<p>You have crossed maximum limit of attempts.</p>`
+        return;
+    }
+    attempt++;
     let val =  input.value;
+    arr.push(val);
     input.value = '';
-    let elem = document.getElementsByClassName('container')[0];
-    let insertElem = document.createElement('div');
-    insertElem.className = "new";    
     if(val == randomValue){
-
+        elem.innerHTML = `<p>Your guess is right</p>`;
+        elem.style.background = "green";
+        elem.style.border = "2px solid green";
+        input.disabled = true;
     } else{
         if(val > randomValue){
-            insertElem.innerHTML = `<p>Your guess is too large</p>`;
-            elem.append(insertElem);
+            elem.innerHTML = `<p>Your guess is too large</p>`;
         }else{
-            insertElem.innerHTML = `<p>Your guess is too small</p>`;
-            elem.append(insertElem);
+            elem.innerHTML = `<p>Your guess is too small</p>`;
         }
     }
+
+    // setTimeout(()=>{
+    //     elem.style.display = "none";
+    // }, 10000)
+    showGuess();
 }
